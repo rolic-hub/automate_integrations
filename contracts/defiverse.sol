b6a75ced6f647f6224fa02bd1e9395ad07daaf09
rolic-hub <enyiomaeze09@gmail.com>
2022-10-29 22:31:07 +0100

    created utilities folder

--- contracts/defiverse.sol
+++ contracts/defiverse.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
-
+import "./utilities/IERC20.sol";
 import "./integrations/LidoFinance.sol";
 import "./integrations/AaveProtocol.sol";
-import "./integrations/Compound.sol";
+import "./integrations/CompoundIntegration.sol";
 import {AutomationRegistryInterface, State, Config} from "@chainlink/contracts/src/v0.8/interfaces/AutomationRegistryInterface1_2.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
@@ -21,10 +21,10 @@ interface KeeperRegistrarInterface {
     ) external;
 }
 
-contract Defiverse is AaveProtocol, Compound, LidoFinance {
-   
+contract Defiverse is AaveProtocol, CompoundIntegration, LidoFinance {
+    constructor() AaveProtocol() CompoundIntegration() LidoFinance() {}
 
-   function registerAndPredictID(uint256 amount) public {
+    function registerAndPredictID(uint256 amount) public {
         (State memory state, Config memory _c, address[] memory _k) = i_registry
             .getState();
         uint256 oldNonce = state.nonce;
@@ -63,15 +63,27 @@ contract Defiverse is AaveProtocol, Compound, LidoFinance {
         }
     }
 
+    function approveFunction(
+        address assest,
+        uint256 amount,
+        address approvee
+    ) public {
+        IERC20(assest).approve(approvee, amount);
+    }
+
+    function transferFunction(
+        address assest,
+        uint256 amount,
+        address to
+    ) public {
+        IERC20(assest).transfer(to, amount);
+    }
+
     function checkUpkeep(bytes calldata checkData)
         external
         view
         returns (bool upkeepNeeded, bytes memory performData)
-    {
-       
-    }
+    {}
 
-     function performUpkeep(bytes calldata performData) external {
-        
-    }
+    function performUpkeep(bytes calldata performData) external {}
 }
---
+++ contracts/utilities/IERC20.sol
@@ -0,0 +1,12 @@
+interface ERC20 {
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function decimals() external view returns (uint);
+
+    function transfer(address _to, uint256 _value) external returns (bool);
+}
